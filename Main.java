4b46b92a4e1ddd498fc02e8343e05aa38592588d
Chris Sonnhalter <cfsonnhalter@gmail.com>
2021-03-22 16:50:49 +0000

    fixed main class

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-class Main {
- public static void main(String args[]) {
-  SwingUtilities.invokeLater(new Runnable() {
-    public void run() {
-       new Counter();
-    }
-  });
- }
+class Main{
+  public static void main(String args[]) {
+    SwingUtilities.invokeLater(new Runnable(){
+      public void run() {
+        new Counter();
+      }
+    });
+  }
 }
